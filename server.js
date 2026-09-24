165333d3a80a1090096721c985fd93461a7c8613
thsab0563-svg <thsab0563@gmail.com>
2026-09-24 05:20:59 +0300

    Initialize package.json for backend project

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "iraq-chat-backend",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": { "start": "node server.js" },
+  "dependencies": {
+    "express": "^4.19.2",
+    "socket.io": "^4.7.5",
+    "sql.js": "^1.10.3",
+    "multer": "^1.4.5-lts.1"
+  }
+}
